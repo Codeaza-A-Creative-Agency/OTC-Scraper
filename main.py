50dedc4667041c4a84d9dd87f67ae1059f3447ab
Mohammed faheem <44580643+faheem77@users.noreply.github.com>
2022-11-08 14:56:27 +0500

    added duplication check wrong company code

--- main.py
+++ main.py
@@ -31,41 +31,46 @@ headers = {
     'sec-fetch-site': 'same-site',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
 }
-cur.execute("SELECT value FROM outstanding_shares")
-values= cur.fetchall()
 def get_data(name):
     params = {
     'symbol': name,
     }
     response = requests.get(f'https://backend.otcmarkets.com/otcapi/company/profile/full/{name}', params=params, headers=headers)
-    resp= response.json()
-    sharesValue=resp['securities'][0]['outstandingShares']
-    share_date= resp['securities'][0]['outstandingSharesAsOfDate']
-    s_date=pd.to_datetime(share_date, unit='ms').to_pydatetime()
-    # save_to_db(sharesValue,name,s_date)
-    for i in values:
-        if i[0] != sharesValue:
+    try:
+        resp= response.json()
+        sharesValue=resp['securities'][0]['outstandingShares']
+        share_date= resp['securities'][0]['outstandingSharesAsOfDate']
+        s_date=pd.to_datetime(share_date, unit='ms').to_pydatetime()
+        cur.execute(f"SELECT value FROM outstanding_shares where company_code='{name}' ORDER BY company_code DESC LIMIT 1")
+        value= cur.fetchone()
+        
+        if value[0] != sharesValue:
+            print(value[0])
+            print(sharesValue)
             save_to_db(sharesValue,name,s_date)
             message = "Subject: Outstanding Shares value changed for " + name +"="+ str(sharesValue)
-            try:
-                smtpObj = smtplib.SMTP(host)
-                smtpObj.sendmail(sender, receivers, message)         
-                print("Successfully sent email")
-            except SMTPResponseException as e:
-                error_code = e.smtp_code
-                error_message = e.smtp_error
-                print(error_code, error_message)
+            return "Successfully added data to database"
+            # try:
+            #     smtpObj = smtplib.SMTP(host)
+            #     smtpObj.sendmail(sender, receivers, message)         
+            #     print("Successfully sent email")
+            # except SMTPResponseException as e:
+            #     error_code = e.smtp_code
+            #     error_message = e.smtp_error
+            #     print(error_code, error_message)
         else:
-            print("No change in outstanding shares")
+            return"No change in outstanding shares"
+
+    except:
+        return "No data found for this company code :" + name
             
            
             
            
-    return "Done"
             
 
 
 
 for name in names:
     # print(name[0])
-    get_data(name[0])
+    print(get_data(name[0]))
